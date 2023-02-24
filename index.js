9cec6f48687fbb7f07ce188afffbed7357cdf2f7
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2023-02-24 11:41:44 +0000

    update port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
     res.send('Welcome to NES Drive API')
